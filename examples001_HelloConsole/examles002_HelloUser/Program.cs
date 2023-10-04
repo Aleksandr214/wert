f3c36cf94e74929e6d9898e669205094e0a5723f
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-04 15:25:08 +0400

    сохронил файл

--- examples001_HelloConsole/examles002_HelloUser/Program.cs
+++ examples001_HelloConsole/examles002_HelloUser/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more inf
-Console.Write("Введите ваше имя");
+﻿Console.Write("Введите ваше имя");
 String Username=Console.ReadLine();
 Console.Write("Привет,");
 Console.Write(Username);
