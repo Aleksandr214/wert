26d03325753991cb57f67113090b90fa9030c90c
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-04 14:12:59 +0400

    добавил папку

---
+++ examples001_HelloConsole/examles002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more inf
+Console.Write("Введите ваше имя");
+String Username=Console.ReadLine();
+Console.Write("Привет,");
+Console.Write(Username);
