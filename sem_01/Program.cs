59e8fab83de68ae22eb3719805d52401d387185c
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-06 19:29:30 +0400

    сохронил

--- sem_01/Program.cs
+++ sem_01/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA < numberB )
+{
+    Console.WriteLine("numberB ");
+}
