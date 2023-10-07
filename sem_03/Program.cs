3056a6d0cd4a0b40a506e65d2ace615027407635
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-07 09:52:02 +0400

    сохронил

---
+++ sem_03/Program.cs
@@ -0,0 +1,10 @@
+﻿// на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = 2;
+
+while (a >= b)
+{
+    Console.Write(b + " ");
+    b = b + 2;
+}
