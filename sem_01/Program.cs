45d89553e7c157fd6b4d45088362d8250d09be19
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-07 09:58:13 +0400

    сохронил

--- sem_01/Program.cs
+++ sem_01/Program.cs
@@ -1,5 +1,6 @@
 ﻿﻿Console.WriteLine("Введите три числа, и узнайте, ");
 Console.WriteLine("какое из них имеет максимальное значение.");
+
 Console.Write("Введите первое число и нажмите ENTER: ");
 int a = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число и нажмите ENTER: ");
