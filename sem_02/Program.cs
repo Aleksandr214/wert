65c5c458cd6336f0b9c174289032237f033ee5a5
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-07 09:17:41 +0400

    сохронение

---
+++ sem_02/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите число");
+
+string number = Console.ReadLine();
+int number1 = int.Parse(number);
+
+
+if (number1 % 2 == 1)
+{
+    Console.WriteLine($"Число {number1} является нечетным");
+}
+else
+{
+    Console.WriteLine($"Число {number1} является четным");
+}
