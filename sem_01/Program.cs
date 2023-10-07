76907172b893c44c2b3ab20be2dfab02c08b583c
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-07 08:24:10 +0400

    сохронил

--- sem_01/Program.cs
+++ sem_01/Program.cs
@@ -1,9 +1,13 @@
-﻿   Console.WriteLine("Введите первое число:");
-
-           int a = Convert.ToInt32(Console.ReadLine());
-
-           Console.WriteLine("Введите второе число:");
-
-           int b = Convert.ToInt32(Console.ReadLine());
-
-           Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
+﻿﻿Console.WriteLine("Введите три числа, и узнайте, ");
+Console.WriteLine("какое из них имеет максимальное значение.");
+Console.Write("Введите первое число и нажмите ENTER: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число и нажмите ENTER: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число и нажмите ENTER: ");
+int c = int.Parse(Console.ReadLine());
+int maxNum = 0;
+if (a > maxNum) maxNum = a;
+if (b > maxNum) maxNum = b;
+if (c > maxNum) maxNum = c;
+Console.Write("Максимальное значение имеет число " + maxNum);
