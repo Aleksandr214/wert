85e6ef6e35f99126d02da1f899428e9df71481c7
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-08 20:21:01 +0400

    сохронил

---
+++ examples009_introArray/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine();
+int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
+array[0] = 12;
+Console.WriteLine(array[0] );
+int max = Max(
+    Max(array[0],array[1],array[2]),
+    Max(array[3],array[4],array[5]),
+    max(array[6],array[7],array[8]));
+
+    console.writeLine(max);
