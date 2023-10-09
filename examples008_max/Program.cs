c4867fcb940372d06a2315efe4059605ffadeec4
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-09 11:30:43 +0400

    сохронил

--- examples008_max/Program.cs
+++ examples008_max/Program.cs
@@ -1,6 +1,10 @@
 ﻿//нахождение максимума из 9 чисел
 
-int Max(int arg1, int arg2, int arg3);
+Console.WriteLine("нахождение максимум из 9 чисел: ");
+
+int Max(int)
+
+int Max(int ard1, int ard2, int ard3)
 {
     int result = arg1;
     if (arg2 > result) result = arg2;
@@ -30,5 +34,8 @@ int c3 = 25;
 //if(c3 > max) max = c3;
 
 int max1 = Max(a1, b1, c1);
+int max1 = Max(a2, b2, c2);
+int max1 = Max(a3, b3, c3);
+int max1 = Max(max1, max2, max3);
 
-Console.WriteLine(max);
+Console.WriteLine(max1);
--- examples009_introArray/Program.cs
+++ examples009_introArray/Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine();
 int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
-array[0] = 12;
-Console.WriteLine(array[0] );
-int max = Max(
-    Max(array[0],array[1],array[2]),
-    Max(array[3],array[4],array[5]),
-    max(array[6],array[7],array[8]));
+array[0] = 11;
+Console.WriteLine(array[8] );
+//int max = Max(
+    //Max(array[0],array[1],array[2]),
+    //Max(array[3],array[4],array[5]),
+    //max(array[6],array[7],array[8]));
 
-    console.writeLine(max);
+    //Console.WriteLine(max);  
