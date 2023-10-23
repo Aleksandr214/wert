a1763884b101f2b8869697fa5a78f6b8ccc7ec00
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-23 13:46:32 +0400

    сохронил

---
+++ examples_013/Program.cs
@@ -0,0 +1,49 @@
+﻿string[,]table=new string[2,5];
+
+
+//table[1,2]="слово";
+//for (int rous = 0; rous < 2; rous++)
+
+//{
+// for (int colons = 0; colons < 5; colons++)
+// {
+//    Console.WriteLine($"{table[rous,colons]}");
+// }   
+//}
+
+//int [,]matrix=new int[3,4];
+int [,] pic=new int [20,21];
+
+void PrintImage(int[,]image)
+{
+
+
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+         //Console.Write($"{image[i, j]} ");
+
+        if(image[i,j]==0)Console.Write($" ");
+        else Console.Write($"+");
+        } 
+        Console.WriteLine();
+    }
+
+}
+//void FillArray(int[,]matr)
+//{
+
+  //  for (int i = 0; i < matr.GetLength(0); i++)
+    //{
+      //  for (int j = 0; j < matr.GetLength(1); j++)
+        //{
+          //  matr[i,j]=new Random().Next(1,10);
+        //}
+    //}
+//}
+//PrintArray(matrix);
+//FillArray(matrix);
+//onsole.WriteLine();
+//PrintArray(matrix);
+PrintImage(pic);
