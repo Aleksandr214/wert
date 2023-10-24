d4a029d4dc5e02910a7ba102242cd62c4e1f9db7
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-24 16:46:18 +0400

    сохронил

--- examples_013/Program.cs
+++ examples_013/Program.cs
@@ -1,4 +1,4 @@
-﻿string[,]table=new string[2,5];
+﻿//string[,]table=new string[2,5];
 
 
 //table[1,2]="слово";
@@ -12,38 +12,51 @@
 //}
 
 //int [,]matrix=new int[3,4];
-int [,] pic=new int[,];
+int[,] pic=new int[23,25];
 
-void PrintImage(int[,]image)
+void PrintImage(int[,] image)
 {
-
-
-    for (int i = 0; i < image.GetLength(0); i++)
+for (int i = 0; i < image.GetLength(0); i++)
+  {
+    for (int j = 0; j < image.GetLength(1); j++)
     {
-        for (int j = 0; j < image.GetLength(1); j++)
-        {
          //Console.Write($"{image[i, j]} ");
 
-        if(image[i,j]==0)Console.Write($" ");
-        else Console.Write($"+");
-        } 
-        Console.WriteLine();
-    }
+      if(image[i,j]==0) Console.Write($" ");
+      else Console.Write($"+");
+     } 
+      Console.WriteLine();
+  }
 
 }
-//void FillArray(int[,]matr)
+
+void FillImage(int row,int col)
+{
+  if(pic[row,col]==0)
+  {
+    pic[row,col]=1;
+    FillImage(row-1,col);
+    FillImage(row,col-1);
+    FillImage(row+1,col);
+    FillImage(row,col+1);   
+  }
+
+}
+PrintImage(pic);
+FillImage(13,13);
+PrintImage(pic);
 //{
 
-  //  for (int i = 0; i < matr.GetLength(0); i++)
+   // for (int i = 0; i < matr.GetLength(0); i++)
     //{
-      //  for (int j = 0; j < matr.GetLength(1); j++)
+      //for (int j = 0; j < matr.GetLength(1); j++)
         //{
-          //  matr[i,j]=new Random().Next(1,10);
+          // matr[i,j]=new Random().Next(1,10);
         //}
-    //}
+   // }
 //}
 //PrintArray(matrix);
 //FillArray(matrix);
-//onsole.WriteLine();
-//PrintArray(matrix);
-PrintImage(pic);
+//Console.WriteLine();
+//PrintArray(image);
+//PrintImage(pic);
