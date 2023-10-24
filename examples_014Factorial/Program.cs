7867b93224e4d42fc2d5dd05d29681245536b547
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-24 17:14:57 +0400

    сохронил

---
+++ examples_014Factorial/Program.cs
@@ -0,0 +1,11 @@
+﻿
+double Factorial(int n) //int поменять на double
+{
+    if(n==0)return 1;
+    else return n*Factorial(n-1);
+
+}
+for (int i = 1; i < 40; i++)
+{
+   Console.WriteLine($"{i}! = {Factorial(i)}"); 
+}
